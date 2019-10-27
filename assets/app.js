d7afb7c84a950d571785235a78d71154479abce6
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-10-27 14:28:20 -0400

    More updates

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-    const displayedBtns = ["Dolphins", "Pandas", "Giraffes"];
+    const displayedBtns = [];
 
     function displayImage(){
         $("#imageContain").empty();
         const input = $(this).attr("data");
         const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=fzRpP1tlDGeKQ5Lt6loKLb7r5PHEtaCXlimit=10";
 
-        $ajax({            
+        $.ajax({            
             url: queryURL,
             method: "GET",
         }).done(function(response){
