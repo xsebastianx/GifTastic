d66ef6f582fe5c3c0fab6f4f9d8a2129bf2a46f2
Sebastian Arnavat <sebastianarnavat@Sebastians-MacBook-Pro.local>
2019-10-26 22:09:02 -0400

    Added functions to JS

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,22 @@ $(document).ready(function(){
     function displayImage(){
         $("#imageContain").empty();
         const input = $(this).attr("data");
-        const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=fzRpP1tlDGeKQ5Lt6loKLb7r5PHEtaCXlimit=10"
+        const queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=fzRpP1tlDGeKQ5Lt6loKLb7r5PHEtaCXlimit=10";
+
+        $ajax({            
+            url: queryURL,
+            method: "GET",
+        }).done(function(response){
+            for (const i = 0; i < limit;i++) {
+                const display = $("<div>")
+                display.addClass("holder")
+
+                const image = $("<img>")
+                image.attr("src", response.data[i].images.original_still.url);
+                image.attr("data-still", response.data[i]).images.original_still.url);
+                image.attr("data-animate", response.data[i].images)
+            }
+
+        })
     }
 })
